123411f76aa7a0287d0ff9305c60534b36fe265a
FUKUDA-RYO-525 <ahr.1783.3b20@gmail.com>
2021-02-28 07:16:06 +0900

    rubocopにて記述変更&2.7.2に変更

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
@@ -33,18 +33,18 @@ module WonderfulEditor
     config.generators.system_tests = nil
 
     config.generators do |g|
-     g.template_engine false
-     g.javascripts false
-     g.stylesheets false
-     g.helper false
-     g.test_framework :rspec,
-                   view_specs: false,
-                   routing_specs: false,
-                   helper_specs: false,
-                   controller_specs: false,
-                   request_specs: true
-     end
-   
-     config.api_only = true
+      g.template_engine false
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
+      g.test_framework :rspec,
+                       view_specs: false,
+                       routing_specs: false,
+                       helper_specs: false,
+                       controller_specs: false,
+                       request_specs: true
+    end
+
+    config.api_only = true
   end
 end
