984da0f02ddbcac0de7adbe064046dee0c6958d7
FUKUDA-RYO-525 <ahr.1783.3b20@gmail.com>
2021-03-03 21:02:08 +0900

    Task. 7-2 base_api_controller の実装

---
+++ app/controllers/api/v1/base_api_controller.rb_controller.rb
@@ -0,0 +1,2 @@
+class Api::V1::BaseApiController.rbController < ApplicationController
+end
---
+++ spec/requests/api/v1/base_api_controller.rb_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::BaseApiController.rbs", type: :request do
+
+end
